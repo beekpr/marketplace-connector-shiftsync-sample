aee36ccabf40e1c2a6047138a13059e10a85f0c9
Kasia Gruenwald <katarzyna.gruenwald@beekeeper.io>
2022-04-28 15:22:32 +0200

    nice logs for demo

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,21 @@
 # syntax = docker/dockerfile:1.0-experimental
 # Build with multistage build on jdk-11
-FROM gradle:6.3.0-jdk11 as java_build
 
-ARG MVN_S3_INTERNAL_ACCESS_KEY
-ARG MVN_S3_INTERNAL_SECRET_KEY
-
-COPY --chown=gradle:gradle . /home/gradle/src
-
-WORKDIR /home/gradle/src
-
-RUN --mount=type=cache,target=/home/gradle/.gradle gradle --parallel -S --no-daemon distTar
 
 ## Build production image with only outputs
-FROM quay.io/beekeeper/integration-connector-base:0.4.13
+FROM integration-connector-base:0.5.9_local
 
-LABEL Description="Sample Shift Sync Connector"
+LABEL Description="Sample Ongoing User Sync Connector"
 
-COPY --from=java_build /home/gradle/src/build/distributions/connector*.tar /opt/bkpr/
-RUN tar -xvf /opt/bkpr/connector*.tar --directory /opt/bkpr  && \
-       rm /opt/bkpr/connector-*.tar
+COPY ./scheduled/build/distributions/connector*.tar /opt/bkpr/
+COPY ./ongoing/build/distributions/connector*.tar /opt/bkpr/
 
-RUN mv /opt/bkpr/connector*/lib/*.jar /opt/bkpr/service/lib/connector/
+RUN tar -xvf /opt/bkpr/connector-ongoing-0.0.1.tar --directory /opt/bkpr  && \
+          rm /opt/bkpr/connector-ongoing-0.0.1.tar
+
+RUN mv -f /opt/bkpr/connector-ongoing-0.0.1/lib/*.jar /opt/bkpr/service/lib/connector/
+
+RUN tar -xvf /opt/bkpr/connector-scheduled-0.0.1.tar --directory /opt/bkpr && \
+          rm /opt/bkpr/connector-scheduled-0.0.1.tar
+
+RUN mv -f /opt/bkpr/connector-scheduled-0.0.1/lib/*.jar /opt/bkpr/service/lib/connector/
---
+++ ongoing/src/main/java/io/beekeeper/connector/shiftsync/sample/SampleOngoingConnector.java
@@ -0,0 +1,67 @@
+package io.beekeeper.connector.shiftsync.sample;
+
+import io.beekeeper.integration.connector.api.OngoingSyncDataProvider;
+import io.beekeeper.integration.connector.api.config.GlobalConfiguration;
+import io.beekeeper.integration.connector.api.exception.ConfigurationException;
+import io.beekeeper.integration.connector.api.exception.PauseFailedException;
+import io.beekeeper.integration.connector.api.exception.ResumeFailedException;
+import io.beekeeper.integration.connector.api.exception.StartupException;
+import io.beekeeper.integration.connector.shiftsync.api.ongoing.OngoingShiftImportConnector;
+import io.beekeeper.integration.connector.shiftsync.api.ongoing.observer.OngoingScheduleImportObserver;
+import io.beekeeper.integration.connector.shiftsync.api.ongoing.observer.OngoingShiftImportObserver;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SampleOngoingConnector implements OngoingShiftImportConnector {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+
+    Thread thread = new Thread() {
+        @SneakyThrows
+        @Override
+        public void run() {
+            int i = 0;
+            while (true) {
+                if (isPaused.get()) {
+                    Thread.sleep(100);
+                } else {
+                    System.out.println(ANSI_RED + "Ongoing sync is running. Message number " + i++ + ANSI_RESET);
+                    Thread.sleep(1000);
+                }
+            }
+        }
+    };
+
+    AtomicBoolean isPaused = new AtomicBoolean();
+
+    @Override
+    public void start(OngoingScheduleImportObserver scheduleObserver, OngoingShiftImportObserver shiftObserver)
+            throws StartupException {
+        thread.start();
+    }
+
+    @Override
+    public void initialize(GlobalConfiguration globalConfiguration, OngoingSyncDataProvider ongoingSyncDataProvider)
+            throws ConfigurationException {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public void pause(String externalTenantId, boolean isThereAnyOtherFullSyncInProgress) throws PauseFailedException {
+        isPaused.set(true);
+        System.out.println(ANSI_RED + "Pausing ongoing import" + ANSI_RESET);
+    }
+
+    @Override
+    public void resume(String externalTenantId, boolean isThereAnyOtherFullSyncInProgress)
+            throws ResumeFailedException {
+        isPaused.set(false);
+        System.out.println(ANSI_RED + "Resuming ongoing import" + ANSI_RESET);
+    }
+}
